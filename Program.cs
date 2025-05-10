2c133c18103a3ee388b137bc1638c2f8340c4dd2
otavio-wenzel <otavio.wenzel@gmail.com>
2025-05-10 18:03:00 -0300

    correção de inconsistencias em controller, configuração do arquivo program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using WebPDV.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AplicacaoDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    )
+);
 
 // Adiciona serviços essenciais ao container
 builder.Services.AddControllers(); // Necessário para suportar controllers
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
 
 var app = builder.Build();
-
-app.UseHttpsRedirection(); // Redireciona HTTP para HTTPS automaticamente
-app.UseAuthorization(); // Habilita autorização se necessário
-app.MapControllers(); // Mapeia os controllers automaticamente
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,10 +31,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
-app.UseAuthorization();
+app.UseHttpsRedirection(); // Redireciona HTTP para HTTPS automaticamente
 
-app.MapControllers();
+app.UseAuthorization(); // Habilita autorização se necessário
+
+app.MapControllers(); // Mapeia os controllers automaticamente
 
 app.Run();
